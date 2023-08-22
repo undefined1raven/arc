f004bb21c08888812684d395f8660c82ea6d8232
raven <zlat.dominic.francisc@gmail.com>
2023-08-23 02:13:01 +0300

    what

--- src/routes/what/+page.svelte
+++ src/routes/what/+page.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
 	import Label from '../../components/common/Label.svelte';
-	import globalStyle from '../../stores/globalStyles';
+	import globalStyle from '../../stores/globalStyles'; 
 </script>
 
 <Label text="fuck you" color={$globalStyle.activeMono} />
